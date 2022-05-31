55ef2eda40a43c4c72d0136ae1509a8e3b96d013
EksdIxD <benny34743@gmail.com>
2022-05-31 12:03:13 +0700

    finished pushHead and pushTail

--- single-linked-list.cpp
+++ single-linked-list.cpp
@@ -1,6 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+struct Node{
+    char name[100];
+    int age;
+    Node* next;
+}*head = NULL, *tail = NULL;
+
+Node* createNewNode(char name[], int age){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    strcpy(newNode -> name, name);
+    newNode -> age = age;
+
+    return newNode;
+}
+
+void pushHead(char name[], int age){
+    Node* temp = createNewNode(name, age);
+
+    if(!head){
+        head = tail = temp;
+    }else{
+        temp -> next = head;
+        head = temp;
+    }
+}
 
 int main(){
 
